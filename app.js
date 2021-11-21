554ea101d959128cea4d3f18a6af8f3bf3265e3e
eBenya <koly_07@mail.ru>
2021-11-21 21:20:16 +0300

    coloring set

--- app.js
+++ app.js
@@ -95,7 +95,7 @@ class DrawPicture{
                 if(isBelongsToSet == 0) {
                     this.setPointInImgData(this.imgData, x, y, {r:0, g:0, b:0, a:255});
                 } else {
-                    this.setPointInImgData(this.imgData, x, y, {r:isBelongsToSet + 10, g:isBelongsToSet, b:isBelongsToSet, a:255});
+                    this.setPointInImgData(this.imgData, x, y, {r:(isBelongsToSet * 17) % 256, g:(isBelongsToSet * 11) % 256, b:255, a:255});
                 }
             } 
         }
